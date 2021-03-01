dd7dd5e8946c8f40043c3e73de2caefd8ee025f0
ChrisH <68413782+superchrisho@users.noreply.github.com>
2021-03-01 16:29:41 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // !exp modules
 import * as React from 'react';
 import axios from 'axios';
-import * as MUI from '@material-ui/core'
 
 // !exp styles
 import './styles/App.css';
@@ -11,32 +10,38 @@ import DisplayData from './components/DisplayData';
 import apiResource from './data/resource';
 
 function App() {
-  // !var states
-  const [data, setData] = React.useState([]);
-  const [query, setQuery] = React.useState(null);
-  const [maxRecords, setMaxRecords] = React.useState(0);
-  const [isLoading, setIsLoading] = React.useState(true);
 
-  const apiUrl = `https://data.gov.sg/api/action/datastore_search?resource_id=`;
+  // !var states
+  const [data, setData] = React.useState();
+  const [query, setQuery] = React.useState(null);
+  const [maxRecords, setMaxRecords] = React.useState();
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [apiUrl, setApiUrl] = React.useState(
+    `https://data.gov.sg/api/action/datastore_search?resource_id=`
+  );
+
+
   // !exp handlers
   const inputHandler = e => {
-    const inputVal = this.value;
-    console.log(inputVal);
+    const inputVal = e.target.value;
     setQuery(inputVal);
   };
 
   const enterHandler = e => {
-    if (e.which === 13) {
-      // fetchQuery();
+    if (e.which == 13) {
+      fetchQuery();
     }
   };
 
   const buttonHandler = () => {
-    // fetchQuery();
+    fetchQuery();
   };
 
   // !exp fetch API
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of e751a8a (updated functions)
   const fetchQuery = async () => {
     if (query) {
       const result = await axios(apiUrl + `&q=${query}&limit=${maxRecords}`);
@@ -45,6 +50,7 @@ function App() {
       setData('');
     }
   };
+<<<<<<< HEAD
 
   const fetchData = async () => {
     apiResource.forEach(ele => {
@@ -71,28 +77,27 @@ function App() {
   };
 =======
 >>>>>>> e751a8ac8959926d1464d3648169ccd6d8713ff7
+=======
+>>>>>>> parent of e751a8a (updated functions)
+
+  const fetchTotal = async () => {
+    let grandTotal=0
+    apiResource.forEach(ele => {
+      axios(`${apiUrl + ele.resourceID}`)
+        .then(result => {
+          let total = result.data.result.total;
+          grandTotal += total;
+          console.log(`Element period: ${ele.period} total: ${total}`)
+          setMaxRecords(grandTotal);
+          setIsLoading(false);
+      }).catch(err=>console.log(err))
+    })
+    
+  };
 
   React.useEffect(() => {
-    let grandTotal = 0;
-    apiResource.forEach(async dataset => {
-      try {
-        const result = await axios(apiUrl + dataset.resourceID);
-        const total = result.data.result.total;
-        grandTotal += total;
-        setMaxRecords(grandTotal);
-        const recordsMax = await axios(
-          apiUrl + dataset.resourceID + `&limit=${total}`
-        );
-        const records = recordsMax.data.result.records;
-        setData(d => [...d, ...records]);
-        
-      } catch (error) {
-        console.log(error);
-      }
-      setIsLoading(false);
-    });
-    
-  }, [apiUrl]);
+    fetchTotal();
+  }, []);
 
   return (
     <div className='App'>
@@ -110,7 +115,7 @@ function App() {
       </div>
 
       {isLoading ? (
-        <MUI.CircularProgress />
+        <p>Loading...</p>
       ) : (
         <DisplayData maxRecords={maxRecords} data={data} />
       )}
